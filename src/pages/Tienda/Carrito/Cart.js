51cd49babbfa517ec77bc34995fbe6d8b56a687e
Sol Ferraro <solagus0501@gmail.com>
2022-07-18 20:00:50 -0300

    "Agregando mensaje al carrito"

--- src/pages/Tienda/Carrito/Cart.js
+++ src/pages/Tienda/Carrito/Cart.js
@@ -6,22 +6,24 @@ import { CartContext } from "../../../context/cartContext";
 function Cart() {
 
     const { cartItem } = useContext(CartContext)
+    const { isInCart } = useContext(CartContext)
 
     return (
         <div className="d-flex justify-content-center carrito">
             <div className="DivCarrito">
                 <h1>Carrito</h1>
-                {cartItem.map((item) => {
+                {isInCart == true ? 
+                cartItem.map((item) => {
                     <div>
                         <p>Articulo: {item.nombre} </p>
                         <p>Cantidad: {item.cantidad} </p>
                         <p>Precio: {item.precio * item.cantidad} </p>
                     </div>
-                })}
+                }) &&
                 <div className="DivFinalizar">
                     <p>Total: </p>
                     <Button variant="danger" className="Button">Terminar compra</Button>
-                </div>
+                </div> : <p>No hay productos en el carrito</p>}
             </div>
         </div>
     )
